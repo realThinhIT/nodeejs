d84cf782152e57dd8b40ee339ea06529a3ddbf39
Thinh Nguyen <thinhnd.ict@gmail.com>
2017-09-17 14:23:03 +0700

    Update require-login middleware

--- app/middlewares/auth/require-login.js
+++ app/middlewares/auth/require-login.js
@@ -17,46 +17,32 @@ const { AuthenticationHelper } = Nodee.Helpers;
 // execute before controller
 export default async (req, res, done) => {
     // insert middleware logic here
-
     let loginInfo;
     try {
-        loginInfo = await AuthenticationHelper.getAuthorizationHeader(req);
-        
+        loginInfo = await AuthenticationHelper.getAuthorizationHeader(this.req);
+
         if (loginInfo.type !== 'bearer') {
-            throw new Error('invalid authentication type');
+            throw new Error();
         }
     } catch (e) {
-        return done(
-            false, 
+        return this.send.fail(
             'invalid authentication type', 
             ErrorCode.http.BAD_REQUEST, 
             DetailCode.auth.INVALID_AUTH_TYPE
         );
     }
 
-    let logIn = new LoginToken();
-    return logIn.findUserByLoginToken(loginInfo.token, (err, isValidated, user) => {
-        if (err || !user || !isValidated) {
-            return done(
-                false, 
-                'invalid access token, token has been disabled or token has expired', 
-                ErrorCode.http.INVALID_CREDENTIALS, 
-                DetailCode.auth.INVALID_ACCESS_TOKEN
-            );
-        }
+    let user;
+    try {
+        user = await (new LoginToken()).findUserByLoginToken(loginInfo.token);
+    } catch (e) {
+        return done(
+            false, 
+            'invalid access token, token has been disabled or token has expired', 
+            ErrorCode.http.INVALID_CREDENTIALS, 
+            e.code
+        );
+    }
 
-        return done(true, user, 200);
-    });
-
-    // AuthenticationHelper.getAuthorizationHeader(req, (err, login) => {
-    //     if (err || login.type !== 'bearer') return done(false, 'invalid authentication type', ErrorCode.http.BAD_REQUEST, DetailCode.auth.INVALID_AUTH_TYPE);
-
-    //     let logIn = new LoginToken();
-
-    //     return logIn.findUserByLoginToken(login.token, (err, isValidated, user) => {
-    //         if (err || !user || !isValidated) return done(false, 'invalid access token, token has been disabled or token has expired', ErrorCode.http.INVALID_CREDENTIALS, DetailCode.auth.INVALID_ACCESS_TOKEN);
-
-    //         return done(true, user, 200);
-    //     });
-    // });
+    return done(true, user, 200);
 };
