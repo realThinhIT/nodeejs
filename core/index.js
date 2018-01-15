aef4a8857b8bdc9a0eb9a09479807bdca9004bdf
Thinh Nguyen <thinhnd.ict@gmail.com>
2018-01-15 15:20:33 +0700

    Add Sequelize to Nodee inject

--- core/index.js
+++ core/index.js
@@ -2,3 +2,4 @@ export { default as Controller } from './base/controller';
 export { default as Exception } from './base/exception';
 export { default as DbDriver } from './base/db-driver';
 export { default as MongooseModel } from './models/mongoose';
+export { default as SequelizeModel } from './models/sequelize';
