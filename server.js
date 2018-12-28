fb256a3d3968074c21a62ed70a14927ef02046a8
Thinh Nguyen <thinhnd.ict@gmail.com>
2018-12-29 02:14:40 +0700

    Fix bug of folder-reconstruction

--- server.js
+++ server.js
@@ -9,6 +9,11 @@
 
 global.__DIR_BASE   = __dirname + '/';
 global.__DIR_APP    = __dirname + '/app/';
-global.__DIR_CORE   = __dirname + '/core/';
+global.__DIR_CORE   = global.__DIR_APP + 'node_modules/nodee/core/';
+
+require('babel-node-modules')([
+  'nodee',
+  'nodee/nodee-model'
+]);
 
 require(__DIR_CORE + 'init');
